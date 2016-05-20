2855a53098feeb953ec577c9099ee005953fd244
Dirkjan Ochtman <dirkjan@ochtman.nl>
2016-05-20 21:49:52 +0200

    Implement handler for keys document

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,20 @@ extern crate iron;
 extern crate openssl;
 extern crate router;
 extern crate serde_json;
+extern crate rustc_serialize;
 
 use std::io::BufReader;
 use std::fs::File;
 use iron::prelude::*;
 use iron::status;
-use openssl::crypto::rsa::RSA;
+use iron::middleware::Handler;
+use openssl::bn::BigNum;
+use openssl::crypto::pkey::PKey;
 use iron::headers::ContentType;
 use router::Router;
 use serde_json::builder::ObjectBuilder;
 use serde_json::value::Value;
+use rustc_serialize::base64::{self, ToBase64};
 
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
@@ -50,15 +54,51 @@ fn oid_config(_: &mut Request) -> IronResult<Response> {
 }
 
 
+fn json_big_num(n: &BigNum) -> String {
+    return n.to_vec().to_base64(base64::URL_SAFE);
+}
+
+
+struct PrivKey {
+    priv_key: PKey,
+}
+
+impl PrivKey {
+    fn new(priv_key: PKey) -> PrivKey {
+        PrivKey { priv_key: priv_key }
+    }
+}
+
+impl Handler for PrivKey {
+    fn handle(&self, _: &mut Request) -> IronResult<Response> {
+        let rsa = self.priv_key.get_rsa();
+        return json_response(&ObjectBuilder::new()
+            .insert_array("keys", |builder| {
+                builder.push_object(|builder| {
+                    builder
+                        .insert("kty", "RSA")
+                        .insert("alg", "RS256")
+                        .insert("use", "sig")
+                        .insert("kid", "base")
+                        .insert("n", json_big_num(&rsa.n().unwrap()))
+                        .insert("e", json_big_num(&rsa.e().unwrap()))
+            })
+        }).unwrap());
+    }
+}
+
+
+
 fn main() {
 
     let priv_key_file = File::open("private.pem").unwrap();
     let mut reader = BufReader::new(priv_key_file);
-    let priv_key = RSA::private_key_from_pem(&mut reader).unwrap();
+    let priv_key = PrivKey::new(PKey::private_key_from_pem(&mut reader).unwrap());
 
     let mut router = Router::new();
     router.get("/", welcome);
     router.get("/.well-known/openid-configuration", oid_config);
+    router.get("/keys.json", priv_key);
     Iron::new(router).http("0.0.0.0:8000").unwrap();
 
 }
