e5de9b44575729721749f81e3889f2b184ef234c
Dan Callahan <dan.callahan@gmail.com>
2016-06-25 08:29:21 -0500

    Use Docopt for CLI arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,51 @@
+extern crate docopt;
 extern crate iron;
 extern crate ladaemon;
-
 #[macro_use(router)]
 extern crate router;
+extern crate rustc_serialize;
 
+use docopt::Docopt;
 use iron::Iron;
-use std::env;
-use std::io::{self, Write};
+
+
+/// Defines the program's version, as set by Cargo at compile time.
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+
+/// Defines the program's usage string.
+///
+/// [Docopt](http://docopt.org) parses this and generates a custom argv parser.
+const USAGE: &'static str = r#"
+Let's Auth.
+
+Usage:
+  ladaemon CONFIG
+  ladaemon --version
+  ladaemon --help
+
+Options:
+  --version       Print version information and exit
+  --help          Print this help message and exit
+"#;
+
+
+/// Holds parsed command line parameters.
+#[derive(RustcDecodable)]
+#[allow(non_snake_case)]
+struct Args {
+    arg_CONFIG: String,
+}
 
 
 /// The `main()` method. Will loop forever to serve HTTP requests.
 fn main() {
-
-    // Make sure we get a file name where we can find configuration.
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        io::stderr().write(b"no configuration file specified\n").unwrap();
-        return;
-    }
+    let args: Args = Docopt::new(USAGE)
+                         .and_then(|d| d.version(Some(VERSION.to_string())).decode())
+                         .unwrap_or_else(|e| e.exit());
 
     // Read the configuration from the provided file.
-    let app = ladaemon::AppConfig::from_json_file(&args[1]);
+    let app = ladaemon::AppConfig::from_json_file(&args.arg_CONFIG);
 
     // Register all handlers with a router object. TODO: cloning the
     // configuration object is kind of ugly, but is apparently needed with
@@ -48,5 +73,4 @@ fn main() {
     // threads according to the number of cores available. TODO: make the
     // interface on which we listen configurable.
     Iron::new(router).http("0.0.0.0:3333").unwrap();
-
 }
