79ce1de82da4c27610f2126ae624e4675f251dd1
Dirkjan Ochtman <dirkjan@ochtman.nl>
2016-07-01 14:03:58 +0200

    Move JWK key set generation into crypto module

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,8 +1,12 @@
 extern crate rand;
 
 use emailaddress::EmailAddress;
+use openssl::bn::BigNum;
 use openssl::crypto::hash;
 use self::rand::{OsRng, Rng};
+use serde_json::builder::ObjectBuilder;
+use serde_json::value::Value;
+use super::AppConfig;
 use rustc_serialize::base64::{self, ToBase64};
 use std::io::Write;
 
@@ -22,4 +26,31 @@ pub fn session_id(email: &EmailAddress, client_id: &str) -> String {
     hasher.write(client_id.as_bytes()).unwrap();
     hasher.write(&rand_bytes).unwrap();
     hasher.finish().to_base64(base64::URL_SAFE)
+}
+
+
+/// Helper function to build a JWK key set JSON Value.
+///
+/// Returns a Value representing the JWK Key Set containing the public
+/// components for the AppConfig's private key, for use in signature
+/// verification.
+pub fn jwk_key_set(app: &AppConfig) -> Value {
+
+    fn json_big_num(n: &BigNum) -> String {
+        n.to_vec().to_base64(base64::URL_SAFE)
+    }
+
+    let rsa = app.priv_key.get_rsa();
+    ObjectBuilder::new()
+        .insert_array("keys", |builder| {
+            builder.push_object(|builder| {
+                builder.insert("kty", "RSA")
+                    .insert("alg", "RS256")
+                    .insert("use", "sig")
+                    .insert("kid", "base")
+                    .insert("n", json_big_num(&rsa.n().unwrap()))
+                    .insert("e", json_big_num(&rsa.e().unwrap()))
+            })
+        })
+        .unwrap()
 }
