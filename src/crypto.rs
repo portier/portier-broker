d68a0022b5900bf376ab3a116e9e8997072c7055
Dirkjan Ochtman <dirkjan@ochtman.nl>
2016-07-01 13:53:31 +0200

    Move session_id() into new crypto module

---
+++ src/crypto.rs
@@ -0,0 +1,25 @@
+extern crate rand;
+
+use emailaddress::EmailAddress;
+use openssl::crypto::hash;
+use self::rand::{OsRng, Rng};
+use rustc_serialize::base64::{self, ToBase64};
+use std::io::Write;
+
+
+/// Helper function to build a session ID for a login attempt.
+///
+/// Put the email address, the client ID (RP origin) and some randomness into
+/// a SHA256 hash, and encode it with URL-safe bas64 encoding. This is used
+/// as the key in Redis, as well as the state for OAuth authentication.
+pub fn session_id(email: &EmailAddress, client_id: &str) -> String {
+    let mut rng = OsRng::new().unwrap();
+    let mut bytes_iter = rng.gen_iter();
+    let rand_bytes: Vec<u8> = (0..16).map(|_| bytes_iter.next().unwrap()).collect();
+
+    let mut hasher = hash::Hasher::new(hash::Type::SHA256);
+    hasher.write(email.to_string().as_bytes()).unwrap();
+    hasher.write(client_id.as_bytes()).unwrap();
+    hasher.write(&rand_bytes).unwrap();
+    hasher.finish().to_base64(base64::URL_SAFE)
+}
