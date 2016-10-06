1d174d9b59a6ff8f2321009cdacf60dd7ee1f45b
Stéphan Kochen <stephan@kochen.nl>
2016-10-06 18:11:00 +0200

    Remove debug JSON response.

--- src/email.rs
+++ src/email.rs
@@ -5,11 +5,8 @@ use emailaddress::EmailAddress;
 use self::lettre::email::EmailBuilder;
 use self::lettre::transport::EmailTransport;
 use self::lettre::transport::smtp::SmtpTransportBuilder;
-use serde_json::builder::ObjectBuilder;
-use serde_json::value::Value;
 use super::{AppConfig, create_jwt};
 use super::crypto::session_id;
-use std::error::Error;
 use std::iter::Iterator;
 use url::percent_encoding::{utf8_percent_encode, QUERY_ENCODE_SET};
 use urlencoded::QueryMap;
@@ -35,7 +32,7 @@ const CODE_CHARS: &'static [char] = &[
 /// authentication, create a randomly-generated one-time pad. Then, send
 /// an email containing a link with the secret. Clicking the link will trigger
 /// the `ConfirmHandler`, returning an authentication result to the RP.
-pub fn request(app: &AppConfig, params: &QueryMap) -> Value {
+pub fn request(app: &AppConfig, params: &QueryMap) {
 
     // Generate a 6-character one-time pad.
     let email_addr = EmailAddress::new(&params.get("login_hint").unwrap()[0]).unwrap();
@@ -46,13 +43,13 @@ pub fn request(app: &AppConfig, params: &QueryMap) -> Value {
     let client_id = &params.get("client_id").unwrap()[0];
     let nonce = &params.get("nonce").unwrap()[0];
     let session = session_id(&email_addr, client_id);
-    let res = app.store.store_session(&session, &[
+    app.store.store_session(&session, &[
         ("email", &email_addr.to_string()),
         ("client_id", client_id),
         ("nonce", nonce),
         ("code", &chars),
         ("redirect", &params.get("redirect_uri").unwrap()[0]),
-    ]);
+    ]).unwrap();
 
     // Generate the URL used to verify email address ownership.
     let href = format!("{}/confirm?session={}&code={}",
@@ -71,27 +68,8 @@ pub fn request(app: &AppConfig, params: &QueryMap) -> Value {
         .build().unwrap();
     // TODO: Add support for authentication.
     let mut mailer = SmtpTransportBuilder::new(app.smtp.address.as_str()).unwrap().build();
-    let result = mailer.send(email);
+    mailer.send(email).unwrap();
     mailer.close();
-
-    // TODO: for debugging, this part returns a JSON response with some
-    // debugging stuff/diagnostics. Instead, it should return a form that
-    // allows the user to enter the code they have received.
-    let mut obj = ObjectBuilder::new();
-    if !result.is_ok() {
-        let error = result.unwrap_err();
-        obj = obj.insert("error", error.to_string());
-        obj = match error {
-            lettre::transport::error::Error::IoError(inner) => {
-                obj.insert("cause", inner.description())
-            }
-            _ => obj,
-        }
-    }
-    if !res.is_ok() {
-        obj = obj.insert("store", res.unwrap_err());
-    }
-    obj.build()
 
 }
 
