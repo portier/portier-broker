572c2c14ecd27cdb705a3f5660f48031d4bc50ee
Dan Callahan <dan.callahan@gmail.com>
2016-11-22 16:52:50 -0600

    Use a Redis script to implement simple ratelimits

--- src/store_limits.rs
+++ src/store_limits.rs
@@ -1,4 +1,4 @@
-use super::redis::{self, PipelineCommands};
+use super::redis::Script;
 use super::error::BrokerResult;
 use super::store::Store;
 
@@ -28,23 +28,20 @@ impl<'a> RatelimitKey<'a> {
     }
 }
 
-
 /// Increment the given key, and test if the counter is within limits.
 pub fn incr_and_test_limits(store: &Store, key: RatelimitKey, ratelimit: &Ratelimit) -> BrokerResult<bool> {
+    let script = Script::new(r"
+        local count = redis.call('incr', KEYS[1])
+        if count == 1 then
+            redis.call('expire', KEYS[1], ARGV[1])
+        end
+        return count
+    ");
 
-    // Note: If quota exceeded, additional requests keep extending the ban.
-    // Thus, with a 5/min ratelimit, it only takes one request each minute to
-    // prevent a ratelimiting from expiring once exceeded.
-    //
-    // TODO? Implement this in Lua to ensure the limit expires every duration.
-    // Pseudocode: return TTL(key) ? INCR(key, 1) : SETEX(key, duration, 1)
-    // Does the INCR above need an EXPIRE to keep keys with TTL=ε from leaking?
-    let k = key.to_string();
-    let (count, ): (usize, ) = redis::pipe()
-        .atomic()
-        .incr(&k, 1)
-        .expire(&k, ratelimit.duration).ignore()
-        .query(&store.client)?;
+    let count: usize = script
+        .key(key.to_string())
+        .arg(ratelimit.duration)
+        .invoke(&store.client)?;
 
-    Ok(count > ratelimit.count)
+    Ok(count <= ratelimit.count)
 }
