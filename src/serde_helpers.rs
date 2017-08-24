f16a6d87f109eb80bad4d9bc67d6100f55ee6811
Stéphan Kochen <stephan@kochen.nl>
2017-08-24 16:50:53 +0200

    Remove unused parts of UrlDef

--- src/serde_helpers.rs
+++ src/serde_helpers.rs
@@ -3,11 +3,10 @@ use serde::de::{Deserialize, Deserializer, Error as DeserializeError};
 use serde::ser::{Serialize, Serializer};
 
 
-/// Type which can be used to serialize a Url.
+/// Unit struct which can be used to serialize a Url.
 ///
 /// `#[serde(with = "UrlDef")]`
-pub struct UrlDef(Url);
-
+pub struct UrlDef;
 impl UrlDef {
     pub fn deserialize<'de, D>(deserializer: D) -> Result<Url, D::Error>
             where D: Deserializer<'de> {
@@ -19,11 +18,5 @@ impl UrlDef {
     pub fn serialize<S>(url: &Url, serializer: S) -> Result<S::Ok, S::Error>
             where S: Serializer {
         url.as_str().serialize(serializer)
-    }
-}
-
-impl From<UrlDef> for Url {
-    fn from(def: UrlDef) -> Url {
-        def.0
     }
 }
