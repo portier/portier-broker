cc3f2435829f1573b572510f4744e95ad943d7b9
Stéphan Kochen <stephan@kochen.nl>
2020-02-23 08:39:57 +0100

    Test more variations using E2E test

--- tests/e2e/src/broker.js
+++ tests/e2e/src/broker.js
@@ -4,31 +4,49 @@ const path = require("path");
 const crypto = require("crypto");
 const { spawn } = require("child_process");
 
+const {
+  RUST_LOG = "error",
+  TEST_REDIS,
+  TEST_SQLITE,
+  TEST_ROTATING_KEYS
+} = process.env;
+
 const ROOT = path.resolve(__dirname, "../../../");
 const BIN = path.resolve(ROOT, "target/debug/portier-broker");
 
 module.exports = () => {
-  const { privateKey } = crypto.generateKeyPairSync("rsa", {
-    modulusLength: 2048
-  });
-  const pem = privateKey.export({
-    type: "pkcs8",
-    format: "pem"
-  });
+  const env = {
+    RUST_LOG: process.env.RUST_LOG || "error",
+    RUST_BACKTRACE: "1",
+    BROKER_PUBLIC_URL: "http://localhost:3333",
+    BROKER_FROM_ADDRESS: "portier@example.com",
+    BROKER_SMTP_SERVER: "localhost:1025",
+    BROKER_LIMIT_PER_EMAIL: "10000/min"
+  };
+
+  if (TEST_REDIS) {
+    env.BROKER_REDIS_URL = "redis://localhost/0";
+  } else if (TEST_SQLITE) {
+    const id = String(Math.random()).slice(2);
+    env.BROKER_SQLITE_DB = `/tmp/portier-broker-test-${id}.sqlite3`;
+  } else {
+    env.BROKER_MEMORY_STORAGE = "true";
+  }
+
+  if (!TEST_ROTATING_KEYS) {
+    const { privateKey } = crypto.generateKeyPairSync("rsa", {
+      modulusLength: 2048
+    });
+    env.BROKER_KEYTEXT = privateKey.export({
+      type: "pkcs8",
+      format: "pem"
+    });
+  }
 
   const subprocess = spawn(BIN, {
     stdio: "inherit",
     cwd: ROOT,
-    env: {
-      RUST_LOG: process.env.RUST_LOG || "error",
-      RUST_BACKTRACE: "1",
-      BROKER_PUBLIC_URL: "http://localhost:3333",
-      BROKER_MEMORY_STORAGE: "true",
-      BROKER_KEYTEXT: pem,
-      BROKER_FROM_ADDRESS: "portier@example.com",
-      BROKER_SMTP_SERVER: "localhost:1025",
-      BROKER_LIMIT_PER_EMAIL: "10000/min"
-    }
+    env
   });
 
   return {
