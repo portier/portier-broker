c0090c1899437129b869b6216c69b09a196939a3
Stéphan Kochen <git@stephank.nl>
2021-10-24 14:31:42 +0200

    Fix sendmail test
    Lettre now captures stderr, so our trick no longer works. We now connect
    our dummy sendmail executable to a HTTP server inside the test harness.

--- tests/e2e/src/sendmail.sh
+++ tests/e2e/src/sendmail.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 # A dummy sendmail executable for testing.
-# The output is parsed in `broker.js`.
 
-echo '-----BEGIN RAW EMAIL-----' >&2
-cat >&2
-echo '-----END RAW EMAIL-----' >&2
+exec curl -fsS -X POST --data-binary @- http://localhost:44920/raw
